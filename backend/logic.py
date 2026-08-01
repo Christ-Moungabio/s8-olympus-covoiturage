7d633d58f14384d71bc2d0be7e53f8ec2f2c236f
Miss-Ed <mandakae0@gmail.com>
2026-08-02 00:39:26 +0100

    fix: utilisation de .get() pour éviter les KeyError sur reservation

--- backend/logic.py
+++ backend/logic.py
@@ -194,12 +194,11 @@ def compter_reservations_par_trajet(trajet_id, reservations):
         la réservation du trajet 2 ne compte pas, ce n'est pas le bon trajet)
     """
     # TODO : à compléter
-    compteur=0
+    compteur = 0
     for reservation in reservations:
-        if reservation["trajet_id"]==trajet_id and reservation["statut"] != "annule":
-            compteur+=1
+        if reservation.get("trajet_id") == trajet_id and reservation.get("statut") != "annule":
+            compteur += 1
     return compteur
-
 
 def verifier_place_disponible(trajet_id, trajets, reservations):
     """
